650dce1b950a5678d81ee1b94dc45aea5539ef7a
TheMrMelc <TheMrMelc@users.noreply.github.com>
2015-05-24 14:23:30 +0200

    Choix du module que l'on souhaite apprendre à utiliser

--- src/mu0Qt/aide.h
+++ src/mu0Qt/aide.h
@@ -10,35 +10,17 @@ namespace Ui {
 
 class Aide : public QDialog {
     Q_OBJECT
-    Q_DISABLE_COPY(Aide)
 public:
     explicit Aide(QWidget *parent = 0);
     virtual ~Aide();
 
-protected:
-    virtual void changeEvent(QEvent *e);
-
 private:
-    Ui::Aide *m_ui;
-    //Phonon::MediaObject *music;
+    Ui::Aide *ui;
 
 private slots:
-    void on_pushButton_18_clicked();
-    void on_pushButton_17_clicked();
-    void on_pushButton_16_clicked();
-    void on_pushButton_15_clicked();
-    void on_pushButton_14_clicked();
-    void on_pushButton_13_clicked();
-    void on_pushButton_12_clicked();
-    void on_pushButton_11_clicked();
-    void on_pushButton_10_clicked();
-    void on_pushButton_9_clicked();
-    void on_pushButton_7_clicked();
-    void on_pushButton_6_clicked();
-    void on_pushButton_5_clicked();
-    void on_pushButton_4_clicked();
-    void on_pushButton_3_clicked();
+    void on_pushButton_clicked();
     void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
 };
 
 #endif // AIDE_H
